7ed8b2afaa1b8047b61935a71944c3683dcab268
Vlad-Gabriel Enachi <vladgabrielenachi@Vlad-Gabriels-MacBook-Pro.local>
2023-12-04 21:27:20 +0200

    chore: add initial project structure

---
+++ .github/workflows/pull_request_template.md
@@ -0,0 +1,3 @@
+#### Short Description
+
+Add a quick overview of what these changes are about
---
+++ src/main/java/com/store/management/tool/StoreManagementToolApplication.java
@@ -0,0 +1,12 @@
+package com.store.management.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(StoreManagementToolApplication.class, args);
+    }
+}
