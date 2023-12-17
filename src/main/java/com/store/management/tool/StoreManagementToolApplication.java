77bb2bce6336d0a9d095e31a1554ada3bc9dcffa
eVoRisk <enachi.vlad.gabriel@gmail.com>
2023-12-17 19:02:30 +0200

    chore: add readme and documentation

--- src/main/java/com/store/management/tool/StoreManagementToolApplication.java
+++ src/main/java/com/store/management/tool/StoreManagementToolApplication.java
@@ -1,11 +1,13 @@
 package com.store.management.tool;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@OpenAPIDefinition
 public class StoreManagementToolApplication {
 
     public static void main(final String[] args) {
